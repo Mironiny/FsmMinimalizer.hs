9eaf94872ec9b77ad1eded0087fe4bbe30aea821
Miroslav Novák <xnovak1k@stud.fit.vutbr.cz>
2018-02-19 23:25:11 +0100

    add parse

--- flp.hs
+++ flp.hs
@@ -1,6 +1,13 @@
 import System.IO
 import System.Environment
 import Data.List
+import Data.List.Split
+
+data Fsm = Fsm { states ::  [String]
+               , initialState :: [String]
+               , finiteState ::  [String]
+               , rules :: [[String]]
+               } deriving (Show)
 
 main = do
     args <- getArgs
@@ -10,8 +17,13 @@ main = do
                     readStdin
                 else
                     readFromFile (head args)
-    print fsm
+    let x = parse fsm
+    printFsm
+    -- print(splitOn "," "my,comma,separated,list")
 
+printFsm = do
+    print "helloWorld"
+    
 readStdin:: IO String
 readStdin = do
     contents <- getContents
@@ -21,3 +33,23 @@ readFromFile :: String -> IO String
 readFromFile filename = do
     contents <- readFile filename
     return contents
+
+-- parse:: String -> Fsm
+parse fsm = Fsm { states = (parsedFsm !! 0)
+                , initialState = (parsedFsm !! 1)
+                , finiteState = (parsedFsm !! 2)
+                , rules = (drop 3 parsedFsm)
+                }
+    where fsmByLines = lines fsm
+          parsedFsm = map (splitOn ",") fsmByLines
+
+
+-- parse fsm =
+--     let fsmByLines = lines fsm
+--         parsedFsm = map (splitOn ",") fsmByLines
+--     in map (splitOn ",") fsmByLines
+    -- in Fsm {states = (head parsedFsm),
+    --         initialState = [],
+    --         finiteState = [],
+    --         rules = [[]]
+    --         }
